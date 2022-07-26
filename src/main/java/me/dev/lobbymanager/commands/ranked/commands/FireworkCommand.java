49847a77f3480dbf1e41b3203c8efec1efc7fdec
Valen Ramos Montero <valen.ramosmontero@gmail.com>
2022-07-26 20:22:36 -0300

    Added /fly and /firework commands

--- src/main/java/me/dev/lobbymanager/commands/ranked/commands/FireworkCommand.java
+++ src/main/java/me/dev/lobbymanager/commands/ranked/commands/FireworkCommand.java
@@ -1,16 +1,38 @@
 package me.dev.lobbymanager.commands.ranked.commands;
 
+import me.dev.lobbymanager.LobbyManager;
+import net.minecraft.server.v1_8_R3.EntityTypes;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FireworkCommand implements CommandExecutor {
+    private static Plugin plugin = LobbyManager.getPlugin();
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-
+            if (p.hasPermission("server.rank.vip")) {
+                    Firework f = (Firework) p.getWorld().spawnEntity(p.getLocation(), EntityType.FIREWORK);
+                    FireworkMeta fm = f.getFireworkMeta();
+                    List<Color> c = new ArrayList<Color>();
+                    c.add(Color.GREEN);
+                    FireworkEffect effect = FireworkEffect.builder().flicker(true).withColor(c).withFade(c).with(FireworkEffect.Type.CREEPER).trail(true).build();
+                    fm.addEffect(effect);
+                    fm.setPower(1);
+                    f.setFireworkMeta(fm);
+            }
         }
         return true;
     }
--- src/main/java/me/dev/lobbymanager/commands/ranked/commands/ToggleFlyCommand.java
+++ src/main/java/me/dev/lobbymanager/commands/ranked/commands/ToggleFlyCommand.java
@@ -1,5 +1,6 @@
 package me.dev.lobbymanager.commands.ranked.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,7 +11,17 @@ public class ToggleFlyCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-            //SET ALLOW FLIGHT
+            if (p.hasPermission("server.rank.vip")) {
+                if (!p.getAllowFlight()) {
+                    p.setAllowFlight(true);
+                    p.sendMessage(ChatColor.GREEN + "Modo de vuelo " + ChatColor.AQUA + "" + ChatColor.BOLD +  "Activado" + ChatColor.GREEN + "!");
+                } else {
+                    p.setAllowFlight(false);
+                    p.sendMessage(ChatColor.GREEN + "Modo de vuelo " + ChatColor.AQUA + "" + ChatColor.BOLD +  "Desactivado" + ChatColor.GREEN + "!");
+                }
+            } else {
+                p.sendMessage(ChatColor.RED + "No puedes hacer eso!");
+            }
         }
         return true;
     }
